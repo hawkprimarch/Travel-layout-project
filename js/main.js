f2aa1d0e70ea639a95cd729f7cc504c474d1b98f
hawkprimarch <hawkprimarch@mail.ru>
2022-09-12 13:17:31 +0500

    burger icon fix

--- js/main.js
+++ js/main.js
@@ -20,8 +20,12 @@ $('.slider__btn--next').click(function() {
 
 // Nav icon
 const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav')
 const menuIcon = document.querySelector('.menu-icon');
 
-navBtn .onclick = function() {
-    menuIcon.classList.toggle('menu-icon-active');
-}
+
+navBtn.onclick = function () {
+	nav.classList.toggle('nav--mobile')
+	menuIcon.classList.toggle('menu-icon--active');
+	document.body.classList.toggle('no-scroll');
+};
